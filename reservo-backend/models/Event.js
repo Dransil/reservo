c1f363fe82c87d8a28c830685f397fa4bfc95b27
Emanuel Taquichiri <thecobra1783@gmail.com>
2026-06-13 23:44:56 -0400

    Modelo de evento

---
+++ reservo-backend/models/Event.js
@@ -0,0 +1,47 @@
+'use strict'
+
+module.exports = (sequelize, DataTypes) => {
+  const Event = sequelize.define('Event', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
+    venue_id: {
+      type: DataTypes.UUID,
+      allowNull: false
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      defaultValue: 0.00
+    },
+    capacity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1
+    },
+    image_url: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    is_active: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    }
+  }, {
+    tableName: 'events',
+    underscored: true
+  })
+
+  return Event
+}
