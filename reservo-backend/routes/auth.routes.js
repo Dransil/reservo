b15fc38582ab83fe61fa41b72122c783d66b3fa1
Emanuel Taquichiri <thecobra1783@gmail.com>
2026-06-15 22:25:18 -0400

    Rutas para auth

--- reservo-backend/routes/auth.routes.js
+++ reservo-backend/routes/auth.routes.js
@@ -1,5 +1,11 @@
 const router = require('express').Router()
+const { register, login, me } = require('../controllers/auth.controller')
+const { registerValidator, loginValidator } = require('../validators/auth.validator')
+const validate = require('../middlewares/validate.middleware')
+const auth = require('../middlewares/auth.middleware')
 
-// TODO
+router.post('/register', registerValidator, validate, register)
+router.post('/login', loginValidator, validate, login)
+router.get('/me', auth, me)
 
 module.exports = router
