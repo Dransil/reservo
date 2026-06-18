c23b7269b08eb4c8e21a66cb6a1ce9bcf5e30e47
Emanuel Taquichiri <thecobra1783@gmail.com>
2026-06-17 22:51:08 -0400

    Rutas para event

--- reservo-backend/routes/event.routes.js
+++ reservo-backend/routes/event.routes.js
@@ -1,5 +1,18 @@
 const router = require('express').Router()
+const { getAll, getOne, create, update, remove } = require('../controllers/event.controller')
+const { eventValidator } = require('../validators/event.validator')
+const validate = require('../middlewares/validate.middleware')
+const auth     = require('../middlewares/auth.middleware')
+const role     = require('../middlewares/role.middleware')
+const upload   = require('../middlewares/upload.middleware')
 
-// TODO
+// Públicas
+router.get('/',    getAll)
+router.get('/:id', getOne)
+
+// Protegidas
+router.post('/',    auth, role('owner', 'admin'), upload.single('image'), eventValidator, validate, create)
+router.put('/:id',  auth, role('owner', 'admin'), upload.single('image'), eventValidator, validate, update)
+router.delete('/:id', auth, role('owner', 'admin'), remove)
 
 module.exports = router
