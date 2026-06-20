ae5ab6a4ae29d6b9bf4a28b051e6776b07a3024e
Emanuel Taquichiri <thecobra1783@gmail.com>
2026-06-19 23:13:28 -0400

    Rutas de booking

--- reservo-backend/routes/booking.routes.js
+++ reservo-backend/routes/booking.routes.js
@@ -1,5 +1,14 @@
 const router = require('express').Router()
+const { getAll, getOne, create, cancel, confirm } = require('../controllers/booking.controller')
+const { bookingValidator } = require('../validators/booking.validator')
+const validate = require('../middlewares/validate.middleware')
+const auth     = require('../middlewares/auth.middleware')
 
-// TODO
+// Todas requieren login
+router.get('/',    auth, getAll)
+router.get('/:id', auth, getOne)
+router.post('/',   auth, bookingValidator, validate, create)
+router.patch('/:id/cancel',  auth, cancel)
+router.patch('/:id/confirm', auth, confirm)
 
 module.exports = router
