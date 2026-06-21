f0fdcac80d7502f6f6d122c67039f498ab0dc024
Emanuel Taquichiri <thecobra1783@gmail.com>
2026-06-20 21:49:18 -0400

    Añadir job al proceso

--- reservo-backend/server.js
+++ reservo-backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const app = require('./app')
 const { connectDB } = require('./config/database')
+const startCancelExpiredJob = require('./jobs/cancelExpired.job')
 
 const PORT = process.env.PORT || 3000
 
